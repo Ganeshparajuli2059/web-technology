d5d90f1ceaa6a370db885103179e1e07ad54e010
Ganeshparajuli2059 <119146652+Ganeshparajuli2059@users.noreply.github.com>
2023-02-24 20:54:38 +0545

    Add files via upload

---
+++ week 1/script.js
@@ -0,0 +1,18 @@
+function signIn() {
+// code to check email and password against database and log user in
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+// check if email and password match
+// if match, redirect to home page
+// else, show error message
+}
+
+function signUp() {
+// code to create new user in database
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+var confirm_password = document.getElementById("confirm_password").value;
+// check if password and confirm_password match
+// if match, create new user
+// else, show error message
+}
